4af16d3162d6153178803a9e6b100fd30c03b6ae
Rui Zhao <ruizhao@Ruis-MacBook-Pro.local>
2016-09-27 22:42:41 -0500

    Refactor middleware

--- server.js
+++ server.js
@@ -2,21 +2,13 @@ var express = require("express");
 var app = express();
 var PORT = 3001;
 
-var middleware = {
-	requireAuthentication:  funtion(req,res,next){
-		console.log("private route hit");
-		next();
-	},
-	logger: function(req,res,next){
-		console.log("Request: " + new Date().toString() + " "+ req.method + " " + req.originalUrl());
-		next();
-	}
 
-};
+var middleware = require("./middleware.js");
+
 
 app.use(middleware.logger);
 app.get("/about", middleware.requireAuthentication,function(req,res){
-	res.send("About us");
+	res.send("About us !");
 });
 
 app.use(express.static(__dirname+'/public'));
