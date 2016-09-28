8ce9a46c5026ba6bc5f644d494287d4fdca556b7
Rui Zhao <ruizhao@Ruis-MacBook-Pro.local>
2016-09-27 23:22:33 -0500

    language

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var middleware = require("./middleware.js");
 
 app.use(middleware.logger);
 app.get("/about", middleware.requireAuthentication,function(req,res){
-	res.send("About us !");
+	res.send("About us ! adn you");
 });
 
 app.use(express.static(__dirname+'/public'));
